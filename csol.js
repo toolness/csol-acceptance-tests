764fc89b96af26a081704ba889b70cd279979292
Atul Varma <varmaa@gmail.com>
2013-06-24 15:00:57 -0400

    'csol.js init' also creates the csol site db if needed.

--- csol.js
+++ csol.js
@@ -4,6 +4,7 @@ var program = require('commander');
 var async = require('async');
 
 var projects = require('./lib/projects');
+var db = require('./lib/db');
 
 program
   .command('init')
@@ -12,7 +13,17 @@ program
     async.series([
       projects['CSOL-site'].exec.bind(null, 'npm install'),
       projects['aestimia'].exec.bind(null, 'npm install'),
-      projects['openbadger'].exec.bind(null, 'npm install')
+      projects['openbadger'].exec.bind(null, 'npm install'),
+      function(cb) {
+        console.log('Creating CSOL MySQL database.');
+        db.create({
+          host: projects['CSOL-site'].env.CSOL_DB_HOST,
+          port: projects['CSOL-site'].env.CSOL_DB_PORT,
+          user: projects['CSOL-site'].env.CSOL_DB_USER,
+          password: projects['CSOL-site'].env.CSOL_DB_PASS,
+          db: projects['CSOL-site'].env.CSOL_DB_NAME
+        }, cb);
+      }
     ], function(err) {
       if (err) throw err;
       console.log("Initialization successful.");
