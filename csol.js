83050899617daabf704d134e50eb376d45c071e5
Atul Varma <varmaa@gmail.com>
2013-06-25 00:38:56 -0400

    start command waits for each server to start up.

--- csol.js
+++ csol.js
@@ -5,6 +5,36 @@ var async = require('async');
 
 var projects = require('./lib/projects');
 var db = require('./lib/db');
+var pingUntilReady = require('./lib/ping-until-ready');
+var serverProcesses = [];
+
+function startServer(project, cmdline, cb) {
+  var serverProcess = project.exec(cmdline, function() {});
+  pingUntilReady(project.url, 3000, cb);
+  serverProcess.on('exit', function(code, signal) {
+    serverProcess.exited = true;
+    if (code != 0)
+      throw new Error("server process '" + cmdline +
+                      "' of " + project.name + " exited with code " + code);
+  });
+  serverProcesses.push(serverProcess);
+  return serverProcess;
+}
+
+function stopServer(serverProcess, cb) {
+  if (serverProcess.exited) return cb();
+  serverProcess.off();
+  serverProcess.on('error', cb);
+  serverProcess.on('exit', cb);
+  serverProcess.kill('SIGKILL');
+}
+
+process.on('uncaughtException', function(err) {
+  async.map(serverProcesses, stopServer, function() {
+    console.error(err.stack);
+    process.exit(1);
+  });
+});
 
 program
   .command('init')
@@ -37,15 +67,12 @@ program
     async.series([
       projects['CSOL-site'].exec.bind(null, 'npm run-script sync-db'),
       projects['CSOL-site'].exec.bind(null, 'npm run-script migrate-db'),
-      function(cb) {
-        async.parallel([
-          projects['CSOL-site'].exec.bind(null, 'node app.js'),
-          projects['aestimia'].exec.bind(null, 'node bin/aestimia.js'),
-          projects['openbadger'].exec.bind(null, 'node app.js'),
-        ], cb);
-      }
+      startServer.bind(null, projects['aestimia'], 'node bin/aestimia.js'),
+      startServer.bind(null, projects['openbadger'], 'node app.js'),
+      startServer.bind(null, projects['CSOL-site'], 'node app.js')
     ], function(err) {
       if (err) throw err;
+      console.log("Services started.");
     });
   });
 
